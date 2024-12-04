39defd009e609e63005f8a688f2ffd5bdd59f691
Oran Donnelly <orandonnelly11@hotmail.com>
2024-12-04 23:12:31 +0000

    First Three Days

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -9,6 +9,19 @@ if __name__ == '__main__':
     data = input_data_from_file('Advent_Day3_Input.txt')
     matches = re.findall(r'mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don\'t\(\)', data)
 
+    #part1
+    count = 0
+    for match in matches:
+        if re.search(r'do\(\)', match):
+            continue
+        elif re.search(r'don\'t\(\)', match):
+            continue
+        else:
+            x, y = int(match.split(',')[0].strip('mul(')), int(match.split(',')[1].strip(')'))
+            count += (x * y)
+
+    print(count)
+
     # part2
     count = 0
     do = True
